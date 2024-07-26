bd7ba43f3496034f8dc8fe97ccd72094b9e512b9
wallace lentes <wallace.lentes@gmail.com>
2024-07-26 18:08:39 -0300

    fix: ajuste do css.

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,7 @@ function copyCode() {
     
     // Criar um elemento temporário para segurar o texto a ser copiado
     var tempInput = document.createElement("textarea");
+    tempInput.style.whiteSpace = "pre-wrap"; // Permitir quebras de linha
     tempInput.value = code;
     document.body.appendChild(tempInput);
     
@@ -17,3 +18,4 @@ function copyCode() {
     // Alerta de confirmação
     alert("Código copiado para a área de transferência!");
 }
+
