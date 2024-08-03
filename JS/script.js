4038e79e014e87c2b02b9b12ecb60327e8fb0de7
wallace lentes <wallace.lentes@gmail.com>
2024-08-02 21:04:43 -0300

    fix: ajustes no button.

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
-function copyCode() {
-    // Obter o texto do elemento <pre>
-    var code = document.getElementById("code").innerText;
+function copyCode(button) {
+    // Obter o texto do elemento <pre> associado ao botão clicado
+    var code = button.previousElementSibling.innerText;
     
     // Criar um elemento temporário para segurar o texto a ser copiado
     var tempInput = document.createElement("textarea");
@@ -18,4 +18,3 @@ function copyCode() {
     // Alerta de confirmação
     alert("Código copiado para a área de transferência!");
 }
-
